e69074f187c1651f4fe0c2d968cbd2eaccd12d37
Thomas Leberbauer <slosd@freedig.org>
2010-08-17 16:16:16 +0200

    -updated menu item behaviour for Firefox 4.0    The menu item appears in the new app menu after the "Options" item by default.    Setting extensions.guiconfig.sticktopreferences removes the item.

--- content/app/gconfig.browser.js
+++ content/app/gconfig.browser.js
@@ -21,18 +21,38 @@ var guiconfig = {
 		var stick = gcCore.MozPreferences.getBoolPref("extensions.guiconfig.sticktopreferences");
 		var tools_menu = document.getElementById("menu_ToolsPopup");
 		var pref_menuitem = document.getElementById("menu_preferences");
+		var app_menuitem = document.getElementById("appmenu_preferences");
 		var gc_menuitem = document.getElementById("gcToolsItem");
+		var gc_appitem = document.getElementById("gcAppItem");
 
-		if(pref_menuitem && stick && gc_menuitem.previousSibling != pref_menuitem) {
-			if(!pref_menuitem.nextSibling)
-				pref_menuitem.parentNode.appendChild(gc_menuitem.cloneNode(true));
-			else
-				pref_menuitem.parentNode.insertBefore(gc_menuitem.cloneNode(true), pref_menuitem.nextSibling);
-			gc_menuitem.parentNode.removeChild(gc_menuitem);
+		if(pref_menuitem) {
+			/* insert the menu item after the "Preferences" item */
+			if(stick && gc_menuitem.previousSibling != pref_menuitem) {
+				if(!pref_menuitem.nextSibling)
+					pref_menuitem.parentNode.appendChild(gc_menuitem.cloneNode(true));
+				else
+					pref_menuitem.parentNode.insertBefore(gc_menuitem.cloneNode(true), pref_menuitem.nextSibling);
+				gc_menuitem.parentNode.removeChild(gc_menuitem);
+			}
 		}
-		else if((!pref_menuitem || !stick) && gc_menuitem.parentNode != tools_menu) {
-			tools_menu.appendChild(gc_menuitem.cloneNode(true));
-			gc_menuitem.parentNode.removeChild(gc_menuitem);
+		else {
+			/* insert the menu item in the "Tools" menu */
+			if(gc_menuitem.parentNode != tools_menu) {
+				tools_menu.appendChild(gc_menuitem.cloneNode(true));
+				gc_menuitem.parentNode.removeChild(gc_menuitem);
+			}
+		}
+		
+		if(app_menuitem) {
+			/* insert the menuitem in the App menu */
+			gc_appitem.style.display = stick ? "-moz-box" : "none";
+			if(stick && gc_appitem.previousSibling != app_menuitem) {
+				if(!app_menuitem.nextSibling)
+					app_menuitem.parentNode.appendChild(gc_appitem.cloneNode(true));
+				else
+					app_menuitem.parentNode.insertBefore(gc_appitem.cloneNode(true), app_menuitem.nextSibling);
+				gc_appitem.parentNode.removeChild(gc_appitem);
+			}
 		}
 		return true;
 	},
