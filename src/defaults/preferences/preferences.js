e3cbc1d48cc579fea681bec5231617a0578fb2a9
Thomas Leberbauer <slosd@freedig.org>
2014-12-16 17:27:17 +0100

    Moved localized descriptions to install manifest

--- src/defaults/preferences/preferences.js
+++ src/defaults/preferences/preferences.js
@@ -1,3 +1,2 @@
-pref("extensions.guiconfig@slosd.net.description", "chrome://guiconfig/locale/gcLocale.properties");
 pref("extensions.guiconfig.todefault.ask", true);
 pref("extensions.guiconfig.matchversion", true);
